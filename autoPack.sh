31d5d0b8829389ad454d965bb8174fb73290e4d7
mining <mining@miningdeiMac-2.local>
2018-08-02 11:18:11 +0800

    finish export adhoc.ipa and release.ipa

--- autoPack.sh
+++ autoPack.sh
@@ -1,27 +1,46 @@
 #自动打包导出脚本，仅适用于XCode9,且工程后缀名为xcodeproj(非cocopods项目)
+#$1 配置文件路径。配置文件中都包含:1、工程路径    2、TeamID   3、archivePath    4、AdHocExportOptionsPlist.plist Path  5、adhoc export path  6、AppStoreExportOptionsPlist.plist Path    7、appsotre export path   8、scheme  ***注意顺序这8个信息的顺序***
 
-echo `date +%Y%m%d_%H%M%S`
-exit
+numberOfLines=$(sed -n '$=' $1) #获取行数
+if [ $numberOfLines -ne 8 ]; then
+    echo "*****缺乏导出必要信息*****"
+    exit
+fi
 
+date=`date +%Y%m%d_%H%M%S`
+projectPath=`sed -n 1p $1` #工程路径 带后缀的xxx/xxx.xcodeproj
+teamID=`sed -n 2p $1`
+archivePath=`sed -n 3p $1`   #带后缀的xxx/xxx.xcarchive
+adhocExportPlistPath=`sed -n 4p $1`
+adhocExportPath=`sed -n 5p $1`
+appStoreExportPlistPath=`sed -n 6p $1`
+appStoreExportPath=`sed -n 7p $1`
+scheme=`sed -n 8p $1`
+
+
+echo "*****正在清理缓存*****"
 xcodebuild clean
 
-xcodebuild -project mipci.xcodeproj \
--scheme mipci \
+echo "*****正在打包*****"
+xcodebuild -project $projectPath \
+-scheme $scheme \
 -configuration Release \
 ONLY_ACTIVE_ARCH=NO \
 CODE_SIGN_IDENTITY="iPhone Developer" \
 CODE_SIGN_STYLE="Automatic" \
 PROVISIONING_STYLE="Automatic" \
-DEVELOPMENT_TEAM="" \
--archivePath ~/Desktop/XCodeB/a.xcarchive \
+DEVELOPMENT_TEAM=$teamID \
+-archivePath $archivePath \
 archive
 
-xcodebuild -exportArchive -archivePath  ~/Desktop/XCodeB/a.xcarchive \
--exportOptionsPlist ./AdHocExportOptionsPlist.plist \
--exportPath ~/Desktop/XCodeB/${appName}_Adhoc
+
+echo "*****正在导出Adhoc包*****"
+xcodebuild -exportArchive -archivePath  $archivePath \
+-exportOptionsPlist $adhocExportPlistPath \
+-exportPath "${adhocExportPath}"
 
 
-
-xcodebuild -exportArchive -archivePath  ~/Desktop/XCodeB/a.xcarchive \
--exportOptionsPlist ./AdHocExportOptionsPlist.plist \
--exportPath ~/Desktop/XCodeB/${appName}_Release
+echo "*****正在导出Release包*****"
+xcodebuild -exportArchive -archivePath  $archivePath \
+-exportOptionsPlist $appStoreExportPlistPath \
+-exportPath "${appStoreExportPath}"
