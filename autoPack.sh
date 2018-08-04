911d1eb6a16df249634c2e40c14e96b888703129
mining <mining@miningdeiMac-2.local>
2018-08-04 09:50:43 +0800

    auto pack and upload shell

--- autoPack.sh
+++ autoPack.sh
@@ -1,8 +1,8 @@
 #自动打包导出脚本，仅适用于XCode9,且工程后缀名为xcodeproj(非cocopods项目)
-#$1 配置文件路径。配置文件中都包含:1、工程路径    2、TeamID   3、archivePath    4、AdHocExportOptionsPlist.plist Path  5、adhoc export path  6、AppStoreExportOptionsPlist.plist Path    7、appsotre export path   8、scheme  ***注意顺序这8个信息的顺序***
+#$1 配置文件路径。配置文件中都包含:1、工程路径    2、TeamID   3、archivePath    4、AdHocExportOptionsPlist.plist Path  5、adhoc export path  6、AppStoreExportOptionsPlist.plist Path    7、appsotre export path   8、scheme   9、账号信息配置文件路径 ***注意顺序这9个信息的顺序***
 
 numberOfLines=$(sed -n '$=' $1) #获取行数
-if [ $numberOfLines -ne 8 ]; then
+if [ $numberOfLines -ne 9 ]; then
     echo "*****缺乏导出必要信息*****"
     exit
 fi
@@ -16,10 +16,19 @@ adhocExportPath=`sed -n 5p $1`
 appStoreExportPlistPath=`sed -n 6p $1`
 appStoreExportPath=`sed -n 7p $1`
 scheme=`sed -n 8p $1`
-
+accountInfoFilePath=`sed -n 9p $1`
+flagStr="SUCCEEDED"
 
 echo "*****正在清理缓存*****"
-xcodebuild clean
+xcodebuild clean -project $projectPath -configuration Release -alltargets  >  ./clean.txt
+
+opreationResult=`cat ./clean.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "${opreationResult}"
+else
+    echo "清理失败"
+    exit
+fi
 
 echo "*****正在打包*****"
 xcodebuild -project $projectPath \
@@ -31,16 +40,52 @@ CODE_SIGN_STYLE="Automatic" \
 PROVISIONING_STYLE="Automatic" \
 DEVELOPMENT_TEAM=$teamID \
 -archivePath $archivePath \
-archive
+archive  >  ./archive.txt
 
+opreationResult=`cat ./archive.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "${opreationResult}"
+else
+    echo "打包失败"
+    exit
+fi
 
 echo "*****正在导出Adhoc包*****"
 xcodebuild -exportArchive -archivePath  $archivePath \
 -exportOptionsPlist $adhocExportPlistPath \
--exportPath "${adhocExportPath}"
+-exportPath "${adhocExportPath}" >  ./exportAdhoc.txt
+
+opreationResult=`cat ./exportAdhoc.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "${opreationResult}"
+else
+    echo "导出Adhoc失败"
+    exit
+fi
 
 
 echo "*****正在导出Release包*****"
 xcodebuild -exportArchive -archivePath  $archivePath \
 -exportOptionsPlist $appStoreExportPlistPath \
--exportPath "${appStoreExportPath}"
+-exportPath "${appStoreExportPath}" >  ./exportRelease.txt
+
+opreationResult=`cat ./exportRelease.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "${opreationResult}"
+else
+    echo "导出Release失败"
+    exit
+fi
+
+
+releaseIpaPath="${appStoreExportPath}/${scheme}.ipa"
+if [ ! -f "$releaseIpaPath" ];  then
+    echo "********************"
+    echo "**                **"
+    echo "**     打包失败     **"
+    echo "**                **"
+    echo "********************"
+    exit
+fi
+
+./upload.sh $releaseIpaPath  $accountInfoFilePath
--- upload.sh
+++ upload.sh
@@ -1,5 +1,37 @@
+#自动检测并上传包，检测和测试结果需要单独加
+#$1为脚本运行的一些必须信息   1.开发者账号     2.账号密码   ***注意顺序这2个信息的顺序***
+
+numberOfLines=$(sed -n '$=' $2) #获取行数
+if [ $numberOfLines -ne 2 ]; then
+    echo "*****缺乏导出必要信息*****"
+    exit
+fi
+
+ipaPath=$1
+account=`sed -n 1p $2`
+password=`sed -n 2p $2`
+flagStr="success-message"
+
 altoolPath="/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+
 echo "*****检测中*****"
-"${altoolPath}" --validate-app -f xxxx -u xxxx -p xxxx -t ios --output-format xml
+"${altoolPath}" --validate-app -f $ipaPath -u $account -p $password -t ios --output-format xml  > ./check.txt
+
+opreationResult=`cat ./check.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "${opreationResult}"
+else
+    echo "检测失败"
+    exit
+fi
+
 echo "*****上传中*****"
-"${altoolPath}" --upload-app -f xxxx -u xxxx -p xxxx -t ios --output-format xml
+"${altoolPath}" --upload-app -f $ipaPath -u $account -p $password -t ios --output-format xml    > ./upload.txt
+
+opreationResult=`cat ./upload.txt | grep $flagStr`
+if [[ "$opreationResult" != "" ]]; then
+    echo "上传结果:${opreationResult}"
+else
+    echo "上传失败"
+    exit
+fi
